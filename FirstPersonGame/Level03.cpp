269377477e7e26083bbf721196cd46bd81f65165
Arawn00 <75842102+Arawn00@users.noreply.github.com>
2023-04-13 16:51:04 +0200

    suite lvl03

--- FirstPersonGame/Level03.cpp
+++ FirstPersonGame/Level03.cpp
@@ -32,14 +32,20 @@ void Level03::Start()
 
     //Sol salle 1 - intérieur avec sauts
     Terrain.push_back(new CubeActor({ 0,0,0 }, { 32.0f,10.0f, 32.0f }, { 239, 123, 69, 255 }));
+    Terrain.push_back(new CubeActor({ -20,0,0 }, { 32.0f,10.0f, 2.0f }, { 239, 123, 69, 255 }));
     Terrain.push_back(new CubeActor({ -40,-20,0 }, { 20.0f,50.0f, 20.0f }, { 239, 123, 69, 255 }));
     Terrain.push_back(new CubeActor({ -40,-20,-40 }, { 20.0f,50.0f, 20.0f }, { 239, 123, 69, 255 }));
-    Terrain.push_back(new CubeActor({ -80,5,-55 }, { 20.0f,5.0f, 3.0f }, { 239, 123, 69, 255 }));
+    Terrain.push_back(new CubeActor({ -70,8,-55 }, { 20.0f,5.0f, 3.0f }, { 239, 123, 69, 255 }));
     Terrain.push_back(new CubeActor({ -100,- 20,-40 }, { 20.0f,50.0f, 20.0f }, { 239, 123, 69, 255 }));
-   
+    Terrain.push_back(new CubeActor({ -140,0,-15 }, { 20.0f,50.0f, 20.0f }, { 239, 123, 69, 255 }));
+    Terrain.push_back(new CubeActor({ -140,0,-50 }, { 20.0f,50.0f, 20.0f }, { 239, 123, 69, 255 }));
+    Terrain.push_back(new CubeActor({ -180,-20,-40 }, { 20.0f,50.0f, 20.0f }, { 239, 123, 69, 255 }));
 
+    Terrain.push_back(new CubeActor({ -180,-20,-110 }, { 20.0f,50.0f, 20.0f }, { 239, 123, 69, 255 }));
+    Terrain.push_back(new CubeActor({ -170,8,-80 }, { 3.0f,5.0f, 20.0f }, { 239, 123, 69, 255 }));
     //mur de la salle 1 
-
+    
+    Terrain.push_back(new CubeActor({ 0,10,20 }, { 32.0f,10.0f, 2.0f }, { 239, 123, 69, 255 }));
 }
 
 
