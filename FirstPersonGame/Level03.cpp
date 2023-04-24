83a33a501c433763fed7bbad6c0ecdbc4d3300ee
Arawn00 <75842102+Arawn00@users.noreply.github.com>
2023-04-24 20:11:46 +0200

    Fixes + polish

--- FirstPersonGame/Level03.cpp
+++ FirstPersonGame/Level03.cpp
@@ -50,30 +50,37 @@ void Level03::Start()
   //Terrain.push_back(new CubeActor({ -32,5,-10 }, { 32.0f,15.0f, 2.0f }, { 192, 192, 192, 255 }));
     //Terrain.push_back(new CubeActor({ 0,20,0 }, { 32.0f,10.0f, 32.0f }, { 239, 123, 69, 255 }));// roof but spawn in 
                                */
-                               //Corridor//
+                               //Corridor Sol//
     Terrain.push_back(new CubeActor({ -180,-20,-130 }, { 20.0f,50.0f, 20.0f }, { 0, 102, 204, 255 }));
-    Terrain.push_back(new CubeActor({ -170,-20,-150 }, { 20.0f,50.0f, 20.0f }, { 0, 102, 204, 255 }));
+    Terrain.push_back(new CubeActor({ -180,-20,-150 }, { 20.0f,50.0f, 20.0f }, { 0, 102, 204, 255 }));
+
+                                //Porte//
+    MovableActor* Door0 = new MovableActor({ -180,8,-120 }, { 7,7,4 }, YELLOW, { 0,10,0 }, 0.5f);
+    MovablesActors.emplace_back(Door0);
+
+                                //Interrupteur//
+    Interuptor* Interuptor0 = new Interuptor({ -173,8,-118 }, { 2,2,2 });
+    Interuptor0->LinkTo(Door0);
+    Interuptors.emplace_back(Interuptor0);
 
                              //Mur Corridor//
-    Terrain.push_back(new CubeActor({ -190,0,-140 }, { 2.0f,32.0f, 48.0f }, { 204 , 255, 255, 255 }));
-    Terrain.push_back(new CubeActor({ -100,0,-140 }, { 2.0f,32.0f, 48.0f }, { 204 , 255, 255, 255 }));
+    Terrain.push_back(new CubeActor({ -190,0,-140 }, { 2.0f,32.0f, 48.0f }, { 204 , 255, 100, 255 }));// H
+    Terrain.push_back(new CubeActor({ -170,0,-140 }, { 2.0f,32.0f, 48.0f }, { 204 , 255, 255, 255 }));// H
+    Terrain.push_back(new CubeActor({ -173,8,-120 }, { 7.0f,10.0f, 2.0f }, { 204 , 255, 255, 255 }));// V
+    Terrain.push_back(new CubeActor({ -187,8,-120 }, { 7.0f,10.0f, 2.0f }, { 204 , 255, 255, 255 }));// V
+
                                  //Pilliers//
-    Terrain.push_back(new CubeActor({ -180,-20,-180 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
-    Terrain.push_back(new CubeActor({ -200,-20,-200 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
-    Terrain.push_back(new CubeActor({ -100,-20,-210 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
-    Terrain.push_back(new CubeActor({ -200,-20,-150 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
+    Terrain.push_back(new CubeActor({ -100,-20,-210 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));// avant dernier
     Terrain.push_back(new CubeActor({ -60,-20,-230 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));// pillier de fin
-    Terrain.push_back(new CubeActor({ -180,-20,-180 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
+    Terrain.push_back(new CubeActor({ -180,-20,-180 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));//premier pillier
     
-    Terrain.push_back(new CubeActor({ -180,-20,-160 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
-    Terrain.push_back(new CubeActor({ -170,-20,-190 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
-    Terrain.push_back(new CubeActor({ -190,-20,-210 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
-    Terrain.push_back(new CubeActor({ -110,-20,-150 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
-    Terrain.push_back(new CubeActor({ -120,-20,-230 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
-    Terrain.push_back(new CubeActor({ -140,-20,-180 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
-    Terrain.push_back(new CubeActor({ -120,-20,-205 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));
-    
-    Terrain.push_back(new CubeActor({ -40,-20,-230 }, { 20.0f,50.0f, 20.0f }, { 0, 204, 0, 255 }));
+   
+    Terrain.push_back(new CubeActor({ -170,-20,-190 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));//2E pillier
+    //Terrain.push_back(new CubeActor({ -120,-20,-230 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 100, 255 }));
+    Terrain.push_back(new CubeActor({ -140,-20,-180 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 }));//3e pillier
+    Terrain.push_back(new CubeActor({ -120,-20,-205 }, { 5.0f,50.0f, 5.0f }, { 153, 51, 255, 255 })); // 4e pillier
+
+    Terrain.push_back(new CubeActor({ -40,-20,-230 }, { 20.0f,50.0f, 20.0f }, { 0, 204, 0, 255 })); // fin 
 }
 
 
